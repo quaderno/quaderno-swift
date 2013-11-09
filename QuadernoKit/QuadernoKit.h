94a30df69c856e0febb5030a8f1f4ad2b792a0cc
Eliezer Talón <elitalon@gmail.com>
2013-11-09 19:00:59 +0000

    Create empty Xcode project

---
+++ QuadernoKit/QuadernoKit.h
@@ -0,0 +1,13 @@
+//
+//  QuadernoKit.h
+//  QuadernoKit
+//
+//  Created by Eliezer Talón on 09/11/13.
+//  Copyright (c) 2013 recrea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QuadernoKit : NSObject
+
+@end
