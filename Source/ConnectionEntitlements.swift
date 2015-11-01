f511f6a13f98e133993f42db89b7179721360846
Eliezer Talón <elitalon@gmail.com>
2015-11-01 20:57:18 +0100

    Makes HTTPHeader public

--- Source/ConnectionEntitlements.swift
+++ Source/ConnectionEntitlements.swift
@@ -71,7 +71,7 @@ extension ConnectionEntitlements {
     - Reset:      Header containing the value for `resetInterval`
     - Remaining:  Header containing the value for `remainingRequests`.
    */
-  enum HTTPHeader: String {
+  public enum HTTPHeader: String {
 
     case Reset = "X-RateLimit-Reset"
     case Remaining = "X-RateLimit-Remaining"
