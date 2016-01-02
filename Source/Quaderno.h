b6f26c1bdf97b425032218beff3ef5d43ad1a330
Eliezer Talón <elitalon@gmail.com>
2016-01-02 20:16:54 +0100

    Update copyright notice

--- Source/Quaderno.h
+++ Source/Quaderno.h
@@ -1,7 +1,7 @@
 //
 // Quaderno.h
 //
-// Copyright (c) 2013-2015 Recrea (http://recreahq.com/)
+// Copyright (c) 2013-2016 Recrea (http://recreahq.com/)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
